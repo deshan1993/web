0946ce2c0caa06b1eb165160c125edf9260ecc27
deshan <sbdeshan@gmail.com>
2018-02-22 11:44:33 +0530

    start up files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: 
+  [
+    
+    '../../assets/home/css/bootstrap.min.css',
+    '../../assets/home/css/main.css',
+      './home.component.scss'
+
+  ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
